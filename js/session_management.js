f3fc472150adbd2d057154d920cbd9bf1bfc11ce
Clarisa Rodríguez <clarisacandia0108@gmail.com>
2023-11-15 00:34:44 -0300

    Modifique session management.

--- js/session_management.js
+++ js/session_management.js
@@ -1,4 +1,4 @@
-function cargarNombreDeUsuarioLogueado() {
+/*function cargarNombreDeUsuarioLogueado() {
     const nodoNombreUsuario = document.getElementById('nombre_usuario');
     const usuarioLogueadoString = localStorage.getItem('usuarioLogueado')
     if (!usuarioLogueadoString) {
@@ -8,6 +8,41 @@ function cargarNombreDeUsuarioLogueado() {
 
     const usuarioLogueadoOBJ = JSON.parse(usuarioLogueadoString)
     nodoNombreUsuario.innerHTML = usuarioLogueadoOBJ.usuario
+
+}*/
+
+function cargarNombreDeUsuarioLogueado() {
+
+    const usuarioLogueadoString = localStorage.getItem('usuarioLogueado')
+    if (usuarioLogueadoString) {
+
+        const usuarioLogueadoOBJ = JSON.parse(usuarioLogueadoString)
+
+        const nodoUsuario = document.getElementById('user_info')
+        const nodoInfoDeUsuario = document.createElement('div')
+        const nodoNombre = document.createElement('p')
+        nodoNombre.innerText = usuarioLogueadoOBJ.usuario
+        const nodoCerrarSesion = document.createElement('a')
+        nodoCerrarSesion.href = 'index.html'
+        nodoCerrarSesion.className = 'cerrar_sesion'
+        nodoCerrarSesion.id = 'cerrar_sesion'
+        nodoCerrarSesion.innerText = 'Cerrar Sesion'
+
+        nodoInfoDeUsuario.appendChild(nodoNombre)
+        nodoInfoDeUsuario.appendChild(nodoCerrarSesion)
+
+        const nodoAvatar = document.createElement('a')
+        nodoAvatar.href = 'perfil.html'
+        const nodoImgAvatar = document.createElement('img')
+        nodoImgAvatar.src = 'img/avatar.png'
+        nodoImgAvatar.className = 'avatar'
+        nodoImgAvatar.alt = 'avatar'
+
+        nodoAvatar.appendChild(nodoImgAvatar)
+        
+        nodoUsuario.appendChild(nodoInfoDeUsuario)
+        nodoUsuario.appendChild(nodoAvatar)
+    }
 
 }
 
